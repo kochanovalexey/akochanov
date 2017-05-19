17231286beee71f467c813290bf016316184dbd0
Kochanov Alexey <dzot-1993@yandex.ru>
2017-05-20 01:48:23 +0300

    Тестовое задание

--- chapter_001/src/main/java/ru/job4j/TestTask.java
+++ chapter_001/src/main/java/ru/job4j/TestTask.java
@@ -17,11 +17,9 @@ public class TestTask {
 	public boolean contains(String origin, String sub) {
 		char[] originCharArray = origin.toCharArray();
 		char[] subCharArray = sub.toCharArray();
-		int counter = 0;
-		for (int i = 0; i + counter < originCharArray.length; i++) {
+		for (int i = 0; i < originCharArray.length - subCharArray.length + 1; i++) {
 			if (originCharArray[i] == subCharArray[0]) {
 				for (int j = 1; j < subCharArray.length; j++) {
-					counter += 1;
 					if (subCharArray[j] != originCharArray[i + j]) {
 						break;
 					} else if (j == subCharArray.length - 1) {
