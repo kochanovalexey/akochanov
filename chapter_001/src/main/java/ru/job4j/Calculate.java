97f22f24469b8ddaa5b23acf3b1f62f7dd103006
Kochanov Alexey <dzot-1993@yandex.ru>
2017-05-26 01:08:25 +0300

    Настройка IDEA

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -6,12 +6,12 @@ package ru.job4j;
  * @author Kochanov Alexey
  * @version $ld$
  * @since 0.1
-*/
+ */
 public class Calculate {
 	/**
 	 * Main.
 	 * @param args - args.
-	*/
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
