93cae278c200f5c22eedecfef2658b111890c20a
Kochanov Alexey <dzot-1993@yandex.ru>
2017-10-31 01:37:21 +0300

    2. Сортировка User с использованием Comparator [#10036]

--- chapter_003/src/main/java/ru/job4j/sortuser/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sortuser/SortUser.java
@@ -1,5 +1,6 @@
 package ru.job4j.sortuser;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -23,5 +24,52 @@ public class SortUser {
             set.add(user);
         }
         return set;
+    }
+
+    /**
+     * Метод сортирующий List User по возрастанию длины имени.
+     * @param userList - список User.
+     * @return - осротированый список User.
+     */
+    public List<User> sortNameLength(List<User> userList) {
+        userList.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        if (o1.getName().length() > o2.getName().length()) {
+                            return 1;
+                        } else if (o1.getName().length() < o2.getName().length()) {
+                            return -1;
+                        } else {
+                            return 0;
+                        }
+                    }
+                }
+        );
+        return userList;
+    }
+    /**
+     * Метод сортирующий List User по двум полям:
+     * 1.возрастанию длины имени,
+     * 2. возрастание возраста.
+     * @param userList - список User.
+     * @return - осротированый список User.
+     */
+    public List<User> sortByAllField(List<User> userList) {
+        userList.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        if (o1.getName().length() > o2.getName().length()) {
+                            return 1;
+                        } else if (o1.getName().length() < o2.getName().length()) {
+                            return -1;
+                        } else {
+                            return o1.compareTo(o2);
+                        }
+                    }
+                }
+        );
+        return userList;
     }
 }
--- chapter_003/src/test/java/ru/job4j/sortuser/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sortuser/SortUserTest.java
@@ -36,4 +36,46 @@ public class SortUserTest {
             assertThat(userResult, is(list.get(truePosition[i++])));
         }
     }
+
+    @Test
+    public void whenSortByLengthName() {
+        SortUser sortUser = new SortUser();
+        List<User> result = new ArrayList<>();
+        User user1 = new User("Sergey", 25);
+        User user2 = new User("Ivan", 30);
+        User user3 = new User("Sergey", 20);
+        User user4 = new User("Ivan", 25);
+        result.add(user1);
+        result.add(user2);
+        result.add(user3);
+        result.add(user4);
+        result = sortUser.sortNameLength(result);
+        List<User> expected = new ArrayList<>();
+        expected.add(user2);
+        expected.add(user4);
+        expected.add(user1);
+        expected.add(user3);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenSortByAllField() {
+        SortUser sortUser = new SortUser();
+        List<User> result = new ArrayList<>();
+        User user1 = new User("Sergey", 25);
+        User user2 = new User("Ivan", 30);
+        User user3 = new User("Sergey", 20);
+        User user4 = new User("Ivan", 25);
+        result.add(user1);
+        result.add(user2);
+        result.add(user3);
+        result.add(user4);
+        result = sortUser.sortByAllField(result);
+        List<User> expected = new ArrayList<>();
+        expected.add(user4);
+        expected.add(user2);
+        expected.add(user3);
+        expected.add(user1);
+        assertThat(result, is(expected));
+    }
 }
